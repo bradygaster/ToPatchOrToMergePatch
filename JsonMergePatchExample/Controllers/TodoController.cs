012e292da20d0969cbbcb2cabd5d39d8bf7f2c92
bradygaster <bradyg@microsoft.com>
2022-04-27 09:26:49 -0700

    Add project files.

---
+++ JsonMergePatchExample/Controllers/TodoController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Morcatko.AspNetCore.JsonMergePatch;
+
+namespace JsonMergePatchExample.Controllers
+{
+    public record Todo(int Id, string Title, bool IsCompleted = false);
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Todo>> Get()
+        {
+            return new ActionResult<List<Todo>>(new List<Todo>(new Todo[] {
+                new Todo(Id: 1, Title: "Test the JsonPatch scenario"),
+                new Todo(Id: 2, Title: "Test the JsonMergePatch scenario")
+            }));
+        }
+
+        [HttpPatch]
+        public ActionResult<Todo> Patch([FromBody] JsonMergePatchDocument<Todo> todo)
+        {
+            return Ok();
+        }
+    }
+}
